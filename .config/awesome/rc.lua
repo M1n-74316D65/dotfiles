34e3057935c6d97e1b4706b615b9995a8922404f
David <gaginodavid@gmail.com>
2022-01-27 13:05:59 +0100

    v2

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,150 +1,96 @@
---       █████╗ ██╗    ██╗███████╗███████╗ ██████╗ ███╗   ███╗███████╗
---      ██╔══██╗██║    ██║██╔════╝██╔════╝██╔═══██╗████╗ ████║██╔════╝
---      ███████║██║ █╗ ██║█████╗  ███████╗██║   ██║██╔████╔██║█████╗
---      ██╔══██║██║███╗██║██╔══╝  ╚════██║██║   ██║██║╚██╔╝██║██╔══╝
---      ██║  ██║╚███╔███╔╝███████╗███████║╚██████╔╝██║ ╚═╝ ██║███████╗
---      ╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+--   ___
+--  /   |_    _____  _________  ________  ___
+--   / /| | | /| / / _ \/ ___/ __ \/ __  __ \/ _ \
+--  / ___ | |/ |/ /  __(__  ) /_/ / / / / / /  __/
+-- /_/  |_|__/|__/\___/____/\____/_/ /_/ /_/\___/
 
+-- AWESOME CONFIG (~/.config/awesome/rc.lua)
+-------------------------------------------------------
+-- Importing libraries
+local gears = require('gears')
+local awful = require('awful')
+local wibox = require('wibox')
+require('awful.autofocus')
+local beautiful = require('beautiful')
+local keys = require('keys')
 
--- Standard awesome libraries
-local gears = require("gears")
-local awful = require("awful")
+-- Variables
+theme_path = string.format('%s/.config/awesome/themes/%s.lua', os.getenv('HOME'), 'default')
+beautiful.init(theme_path)
 
-
--- ===================================================================
--- User Configuration
--- ===================================================================
-
-
-local themes = {
-   "pastel", -- 1
-   "mirage"  -- 2
-}
-
--- change this number to use the corresponding theme
-local theme = themes[1]
-local theme_config_dir = gears.filesystem.get_configuration_dir() .. "/configuration/" .. theme .. "/"
-
--- define default apps (global variable so other components can access it)
-apps = {
-   network_manager = "", -- recommended: nm-connection-editor
-   power_manager = "", -- recommended: xfce4-power-manager
-   terminal = "alacritty",
-   launcher = "rofi -normal-window -modi drun -show drun -theme " .. theme_config_dir .. "rofi.rasi",
-   lock = "i3lock",
-   screenshot = "scrot -e 'mv $f ~/Pictures/ 2>/dev/null'",
-   filebrowser = "nautilus"
-}
-
--- define wireless and ethernet interface names for the network widget
--- use `ip link` command to determine these
-network_interfaces = {
-   wlan = 'wlp1s0',
-   lan = 'enp1s0'
-}
-
--- List of apps to run on start-up
-local run_on_start_up = {
-   "picom --experimental-backends --config " .. theme_config_dir .. "picom.conf",
-   "redshift",
-   "unclutter"
-}
-
-
--- ===================================================================
--- Initialization
--- ===================================================================
-
-
--- Import notification appearance
-require("components.notifications")
-
--- Run all the apps listed in run_on_start_up
-for _, app in ipairs(run_on_start_up) do
-   local findme = app
-   local firstspace = app:find(" ")
-   if firstspace then
-      findme = app:sub(0, firstspace - 1)
-   end
-   -- pipe commands to bash to allow command to be shell agnostic
-   awful.spawn.with_shell(string.format("echo 'pgrep -u $USER -x %s > /dev/null || (%s)' | bash -", findme, app), false)
-end
-
--- Import theme
-local beautiful = require("beautiful")
-beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/" .. theme .. "-theme.lua")
-
--- Initialize theme
-local selected_theme = require(theme)
-selected_theme.initialize()
-
--- Import Keybinds
-local keys = require("keys")
-root.keys(keys.globalkeys)
-root.buttons(keys.desktopbuttons)
-
--- Import rules
-local create_rules = require("rules").create
-awful.rules.rules = create_rules(keys.clientkeys, keys.clientbuttons)
-
--- Define layouts
+-- Layouts
 awful.layout.layouts = {
-   awful.layout.suit.tile,
-   awful.layout.suit.floating,
-   awful.layout.suit.max,
+  --awful.layout.suit.fair,
+  awful.layout.suit.tile,
+  --awful.layout.suit.spiral.dwindle,
+  --awful.layout.suit.max,
+  --awful.layout.suit.magnifier,
+  --awful.layout.suit.tile.bottom,
+  --awful.layout.suit.tile.top,
+  --awful.layout.suit.corner.nw,
+  --awful.layout.suit.fair.horizontal,
+  --awful.layout.suit.spiral,
+  --awful.layout.suit.max.fullscreen,
+  --awful.layout.suit.corner.ne,
+  --awful.layout.suit.corner.sw,
+  --awful.layout.suit.corner.se,
+  awful.layout.suit.floating,
 }
 
--- remove gaps if layout is set to max
-tag.connect_signal('property::layout', function(t)
-   local current_layout = awful.tag.getproperty(t, 'layout')
-   if (current_layout == awful.layout.suit.max) then
-      t.gap = 0
-   else
-      t.gap = beautiful.useless_gap
-   end
+-- Virtual desktops/ Tabs
+awful.screen.connect_for_each_screen(function(s)
+  local tagTable = {'1', '2', '3', '4', '5'}
+
+  -- Uncomment this if not using custom tag names (no. of tags will be derived from `tags` variable set in keys.lua)
+  -- Also uncomment `keys.tags = tags` line in the Variables section in keys.lua!
+  local tagTable = {}
+  for i = 1, keys.tags do
+    table.insert(tagTable, tostring(i))
+  end
+
+  awful.tag(tagTable, s, awful.layout.layouts[1])
 end)
 
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function (c)
-   -- Set the window as a slave (put it at the end of others instead of setting it as master)
-   if not awesome.startup then
-      awful.client.setslave(c)
-   end
+awful.rules.rules = {
+  -- All windows
+  { rule = { },
+    properties = { border_width = beautiful.border_width,
+           focus = awful.client.focus.filter,
+           raise = true,
+           keys = keys.clientkeys,
+           buttons = keys.clientbuttons,
+           screen = awful.screen.preferred,
+           placement = awful.placement.no_overlap+awful.placement.no_offscreen
+           }
+  },
 
-   if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-      -- Prevent clients from being unreachable after screen count changes.
-      awful.placement.no_offscreen(c)
-   end
+  -- Floating exceptions
+  { rule_any = {
+      class = {'Lxappearance', 'qt5ct'},
+      name = {'Event Tester'}, --xev
+      role = {'pop-up', 'GtkFileChooserDialog'},
+      type = {'dialog'}
+    },
+    properties = {floating = true}
+  }
+}
+
+-- Enable sloppy focus
+client.connect_signal('mouse::enter', function(c)
+  c:emit_signal('request::activate', 'mouse_enter', {raise = false})
 end)
 
+-- Colored borders
+client.connect_signal('focus', function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal('unfocus', function(c) c.border_color = beautiful.border_normal end)
 
--- ===================================================================
--- Client Focusing
--- ===================================================================
+-- Autostart
+awful.spawn.with_shell('redshift -x && redshift -O 4000K')
+awful.spawn.with_shell('$HOME/.config/polybar/launch.sh')
+awful.spawn.with_shell('killall dunst; dunst')
+awful.spawn.with_shell('picom -b')
+awful.spawn.with_shell('feh --bg-fill -r -z ~/Pictures/Wallpapers')
 
-
--- Autofocus a new client when previously focused one is closed
-require("awful.autofocus")
-
--- Focus clients under mouse
-client.connect_signal("mouse::enter", function(c)
-   c:emit_signal("request::activate", "mouse_enter", {raise = false})
-end)
-
-
--- ===================================================================
--- Screen Change Functions (ie multi monitor)
--- ===================================================================
-
-
--- Reload config when screen geometry changes
-screen.connect_signal("property::geometry", awesome.restart)
-
-
--- ===================================================================
--- Garbage collection (allows for lower memory consumption)
--- ===================================================================
-
-
-collectgarbage("setpause", 110)
-collectgarbage("setstepmul", 1000)
+-- Garbage Collection
+collectgarbage('setpause', 110)
+collectgarbage('setstepmul', 1000)
--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -42,25 +42,6 @@ lvim.plugins = {
     cmd = "TroubleToggle",
   },
   {
-  "karb94/neoscroll.nvim",
-  event = "WinScrolled",
-  config = function()
-  require('neoscroll').setup({
-        -- All these keys will be mapped to their corresponding default scrolling animation
-        mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-        '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-        hide_cursor = true,          -- Hide cursor while scrolling
-        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = nil,        -- Default easing function
-        pre_hook = nil,              -- Function to run before the scrolling animation starts
-        post_hook = nil,              -- Function to run after the scrolling animation ends
-        })
-  end
-  },
-  {
 		"ethanholz/nvim-lastplace",
 		event = "BufRead",
 		config = function()
@@ -107,9 +88,10 @@ vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
 local cmp = require "cmp"
-lvim.colorscheme = 'everforest'
+lvim.colorscheme = 'gruvbox-material'
 vim.opt.guifont = "FiraCode Nerd Font:h10"
-lvim.background = 'light'
+vim.g.gruvbox_material_background="soft"
+vim.g.background = "light"
 lvim.builtin.cmp.mapping["<C-e>"] = function(fallback)
 vim.g.everforest_background = 'hard'
   cmp.mapping.abort()
