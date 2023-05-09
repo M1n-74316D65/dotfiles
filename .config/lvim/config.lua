ab6c576bc3ff7cd3cc2e0bd62663d7b2d72fb9df
M1n-4d316e <54779580+M1n-4d316e@users.noreply.github.com>
2023-05-10 01:23:57 +0200

    lvim config updated

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,104 +1,17 @@
 lvim.plugins = {
-  {"sainnhe/gruvbox-material"},
-  {"shaunsingh/nord.nvim"},
-  {"sbdchd/neoformat"},
-  {"sainnhe/everforest"},
-  {"github/copilot.vim"},
-  {"glacambre/firenvim",
-    run = function() vim.fn['firenvim#install'](0) end
-  },
+  { "shaunsingh/nord.nvim" },
+  { "sbdchd/neoformat" },
   {
-  'wfxr/minimap.vim',
-  run = "cargo install --locked code-minimap",
-  -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
-  config = function ()
-    vim.cmd ("let g:minimap_width = 10")
-    vim.cmd ("let g:minimap_auto_start = 1")
-    vim.cmd ("let g:minimap_auto_start_win_enter = 1")
-  end,
-  },
-  {
-  "nacro90/numb.nvim",
-  event = "BufRead",
-  config = function()
-  require("numb").setup {
-    show_numbers = true, -- Enable 'number' for the window while peeking
-    show_cursorline = true, -- Enable 'cursorline' for the window while peeking
-  }
-  end,
-  },
-  {
-  "folke/lsp-colors.nvim",
-  event = "BufRead",
-  },
-  {
-  "tzachar/cmp-tabnine",
-  run = "./install.sh",
-  requires = "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  },
-   {
-  "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  {
-		"ethanholz/nvim-lastplace",
-		event = "BufRead",
-		config = function()
-			require("nvim-lastplace").setup({
-				lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-				lastplace_ignore_filetype = {
-					"gitcommit", "gitrebase", "svn", "hgcommit",
-				},
-				lastplace_open_folds = true,
-			})
-		end,
-	},
-  {
-  "folke/todo-comments.nvim",
-  event = "BufRead",
-  config = function()
-    require("todo-comments").setup()
-  end,
-  },
-  {
-  "itchyny/vim-cursorword",
-    event = {"BufEnter", "BufNewFile"},
+    "folke/todo-comments.nvim",
+    event = "BufRead",
     config = function()
-      vim.api.nvim_command("augroup user_plugin_cursorword")
-      vim.api.nvim_command("autocmd!")
-      vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
-      vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
-      vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
-      vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
-      vim.api.nvim_command("augroup END")
-      end
-},
+      require("todo-comments").setup()
+    end,
+  },
+  {
+    'tzachar/cmp-tabnine',
+    build = './install.sh',
+    dependencies = 'hrsh7th/nvim-cmp',
+  }
 }
-lvim.builtin.which_key.mappings["t"] = {
-  name = "Diagnostics",
-  t = { "<cmd>TroubleToggle<cr>", "trouble" },
-  w = { "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", "workspace" },
-  d = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "document" },
-  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
-  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
-}
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_assume_mapped = true
-vim.g.copilot_tab_fallback = ""
-local cmp = require "cmp"
-lvim.colorscheme = 'gruvbox-material'
 vim.opt.guifont = "FiraCode Nerd Font:h10"
-vim.g.gruvbox_material_background="soft"
-vim.g.background = "light"
-lvim.builtin.cmp.mapping["<C-e>"] = function(fallback)
-vim.g.everforest_background = 'hard'
-  cmp.mapping.abort()
-  local copilot_keys = vim.fn["copilot#Accept"]()
-  if copilot_keys ~= "" then
-    vim.api.nvim_feedkeys(copilot_keys, "i", true)
-  else
-    fallback()
-  end
-end
