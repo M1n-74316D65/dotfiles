99aa966f085c6e1c92fda04011e0e8b6ff6a1929
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-05-29 23:48:09 +0200

    feat: add lvim config

---
+++ .config/lvim/config.lua
@@ -0,0 +1,24 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  { "ellisonleao/gruvbox.nvim" },
+  { "andweeb/presence.nvim" },
+
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({
+      })
+    end
+  },
+}
+
+lvim.colorscheme = "gruvbox"
