f9fab7bd5cddb5590dbf3dfc8cb74c5f1ab8bcee
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-07-05 20:18:36 +0200

    feat: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = "Gruvbox Material (Gogh)"
+-- config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font({
+  family = "CommitMono",
+  -- harfbuzz_features = {"zero", "cv04", "cv02", "ss05", "ss04"},
+  harfbuzz_features = { "zero", "cv04", "cv02", "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+})
+
+-- config.enable_tab_bar = false
+-- config.window_decorations = "NONE"
+
+-- and finally, return the configuration to wezterm
+return config
