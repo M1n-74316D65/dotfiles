a63a56a47d3d3eafef007a08f0ac67cebb5ece2e
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-07-06 02:55:09 +0200

    feat: remove wezterm confirm

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,12 +10,14 @@ config.color_scheme = "Gruvbox Material (Gogh)"
 -- config.font = wezterm.font("FiraCode Nerd Font")
 config.font = wezterm.font({
   family = "CommitMono",
-  -- harfbuzz_features = {"zero", "cv04", "cv02", "ss05", "ss04"},
   harfbuzz_features = { "zero", "cv04", "cv02", "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
 })
 
--- config.enable_tab_bar = false
--- config.window_decorations = "NONE"
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+config.enable_wayland = false
+config.default_prog = { 'zellij' }
+config.window_close_confirmation = 'NeverPrompt'
 
 -- and finally, return the configuration to wezterm
 return config
