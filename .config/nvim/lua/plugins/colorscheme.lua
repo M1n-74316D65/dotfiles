be939782c819963849a4ae8569f7c7b0a7df152a
m1n <git@m1n.land>
2025-05-14 19:00:08 +0200

    fix: nvim color based on system

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -10,4 +10,17 @@ return {
       colorscheme = "gruvbox",
     },
   },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+      end,
+      update_interval = 3000, -- milisegundos
+      fallback = "dark",
+    },
+  },
 }
